f909020c2b32e20859d8e0570c4e184298100180
rifkyNur <ythrifky619@gmail.com>
2025-06-14 12:59:18 +0700

    create login and signup page

---
+++ pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GenderCheckbox = () => {
+  return <div>GenderCheckbox</div>;
+};
+
+export default GenderCheckbox;
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/login/Login";
+import SignUp from "../pages/signup/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="flex items-center justify-center p-4 h-screen w-full ">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
